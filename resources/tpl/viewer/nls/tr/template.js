db0243b41d0a5cae93738a422f32e2951ec2dac4
Lucas Cotner <lcotner@blueraster.com>
2015-11-10 17:23:41 -0500

    Initial commit with the esri Map-Series template

---
+++ resources/tpl/viewer/nls/tr/template.js
@@ -0,0 +1,72 @@
+﻿define(
+	 ({
+		viewer: {
+			common: {
+				close: "Kapat"
+			},
+			loading: {
+				long: "Uygulama başlatılıyor",
+				long2: "Beklendiğiniz için teşekkürler",
+				failButton: "Uygulamayı yeniden yükle"
+			},
+			signin: {
+				title: "Kimlik doğrulama gerekli",
+				explainViewer: "Uygulamaya erişmek için %PORTAL_LINK% üzerindeki bir hesapla oturum açın.",
+				explainBuilder: "Uygulamayı yapılandırmak için %PORTAL_LINK% üzerindeki bir hesapla oturum açın."
+			},
+			errors: {
+				boxTitle: "Bir hata oluştu",
+				invalidConfig: "Geçersiz yapılandırma",
+				invalidConfigNoApp: "Web Mapping Application tanımlayıcısı index.html dosyasında tanımlı değil.",
+				unspecifiedConfigOwner: "Yetkili malik yapılandırılmamış.",
+				invalidConfigOwner: "Uygulama sahibi yetkili değil.",
+				createMap: "Harita oluşturulamıyor",
+				invalidApp: "%TPL_NAME% mevcut değil ya da erişilemez.",
+				appLoadingFail: "Hata oluştu, %TPL_NAME% doğru şekilde yüklenmedi.",
+				notConfiguredDesktop: "Uygulama henüz yapılandırılmadı.",
+				notConfiguredMobile: "\"%TPL_NAME%\" oluşturucusu bu ekran çözünürlüğünde desteklenmiyor.",
+				notAuthorized: "Bu uygulamaya erişim yetkiniz yok",
+				noBuilderIE: "Oluşturucu %VERSION% öncesindeki Internet Explorer sürümlerinde desteklenmiyor. %UPGRADE%",
+				noViewerIE: "Bu uygulama %VERSION% öncesindeki Internet Explorer sürümlerinde desteklenmiyor. %UPGRADE%",
+				upgradeBrowser: "<a href='http://browsehappy.com/' target='_blank'>Tarayıcınızı güncelleyin</a>.",
+				mapLoadingFail: "Hata oluştu, harita düzgün şekilde yüklenmedi.",
+				signOut: "Oturumu Kapat"
+			},
+			mobileInfo: {
+				legend: "Gösterim",
+				description: "Açıklama",
+				lblLegendMobileError: "Üzgünüz, gösterim kullanılamıyor. Uygulamayı yeniden yükleyin.",
+				lblLegendMobileErrorExplain: "Uygulama yüklendikten sonra cihaz dikey moda döndürülürse gösterim kullanılamaz."
+			},
+			mobileFooter: {
+				swipeInvite: "Öyküde gezinmek için kaydır",
+				lblNext: "Sonraki",
+				lblEnd: "Öykünün sonuna geldiniz"
+			},
+			headerFromCommon: {
+				storymapsText: "Öykü haritası",
+				builderButton: "Düzenle",
+				facebookTooltip: "Facebook\\'ta Paylaş",
+				twitterTooltip: "Twitter\\'da Paylaş",
+				bitlyTooltip: "Kısa bağlantı edin",
+				templateTitle: "Şablon başlığını ayarla",
+				templateSubtitle: "Şablon altyazısını ayarla",
+				share: "Paylaş"
+			},
+			overviewFromCommon: {
+				title: "Genel Bakış Haritası"
+			},
+			legendFromCommon: {
+				title: "Gösterim"
+			},
+			shareFromCommon: {
+				copy: "Kopyala",
+				copied: "Kopyalandı",
+				open: "Aç",
+				embed: "Web sayfasının içine yerleştir",
+				embedExplain: "Uygulamayı bir web sayfasının içine yerleştirmek için aşağıdaki HTML kodunu kullanın.",
+				size: "Boyut (genişlik/yükseklik):"
+			}
+        }
+    })
+);
